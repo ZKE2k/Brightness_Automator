91121ed7e0e567235834d258177212e10ae5466a
kib <kib@192.168.0.11>
2022-11-03 08:18:06 +0000

    App

---
+++ src/javaapplication9/Looper.java
@@ -0,0 +1,203 @@
+package javaapplication9;
+
+import java.io.File;
+import javax.imageio.ImageIO;
+import java.io.IOException;
+import com.github.sarxos.webcam.Webcam;
+import java.awt.image.BufferedImage;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.concurrent.atomic.AtomicBoolean; 
+import java.time.*;
+
+// initializes class and implements runnable format.
+public class Looper implements Runnable {
+
+    //variable for checking if program is running
+    private AtomicBoolean keepRunning;
+
+    // loops the program
+    public Looper() {
+        keepRunning = new AtomicBoolean(true);
+    }
+
+    // stops the loop
+    public void stop() {
+        keepRunning.set(false);
+    }
+    
+    // pauses the program for an input amount of milliseconds.
+     public static void wait(int ms)
+    {
+        try
+        {
+            Thread.sleep(ms);
+        }
+        catch(InterruptedException ex)
+        {
+            Thread.currentThread().interrupt();
+        }
+    }
+    
+     // stores the max and min brightness values.
+    static int maxb = 100;
+    static int minb = 1;
+    
+    // updates the max and min brightness values.
+    static void maxupd(int max) {
+        maxb = max;
+    }
+    static void minupd(int min) {
+        minb = min;
+    }
+    
+    // initiates the brightness change using a string variable as a powershell command.
+    static void change(int brightness) throws IOException {
+        //Creates a powerShell command that will set the brightness to the requested value (0-100), after the requested delay (in milliseconds) has passed.
+        String s = String.format("$brightness = %d;", brightness)
+                + "$delay = 0;"
+                + "$myMonitor = Get-WmiObject -Namespace root\\wmi -Class WmiMonitorBrightnessMethods;"
+                + "$myMonitor.WmiSetBrightness($delay, $brightness)";
+        String command = "powershell.exe  " + s;
+        // Executing the command
+        Process powerShellProcess = Runtime.getRuntime().exec(command);
+
+        powerShellProcess.getOutputStream().close();
+
+        //Report any error messages
+        String line;
+
+        BufferedReader stderr = new BufferedReader(new InputStreamReader(
+                powerShellProcess.getErrorStream()));
+        line = stderr.readLine();
+        if (line != null)
+        {
+            System.err.println("Standard Error:");
+            do
+            {
+                System.err.println(line);
+            } while ((line = stderr.readLine()) != null);
+
+        }
+        stderr.close();
+
+    }
+    
+    // gets image brightness using an iterating for loop to count each pixel and average the brightness.
+    public static void lum(String ffile) {
+        BufferedImage img=null;
+        
+        File myFile = new File(ffile);
+        
+        try {
+            if (myFile.isFile()) {
+                img = ImageIO.read(myFile);
+                int width=0;
+                int height=0;
+                int count=0;
+                int avg=0;
+                    if(img!=null){
+                        width = img.getWidth();
+                        height = img.getHeight(); 
+                    }
+                for(int x=0; x<width; x++){
+                    for (int y=0;y<height;y++) {
+                        int pixelCol = img.getRGB(x, y);
+                        int a = (pixelCol >>> 24)  & 0xff;
+                        int r = (pixelCol >>> 16)  & 0xff;
+                        int g = (pixelCol >>> 8)  & 0xff;
+                        int b = pixelCol & 0xff;
+                        
+                        double lum = (0.2125*r) + (0.7152*g) + (0.0722*b);
+                        avg+= lum;
+                        count++;
+                    }
+                }
+                avg = avg/count;
+                double br =(double) avg / 255 * 100;
+                int brightness = (int) br;
+                if (brightness < minb){
+                    brightness = minb;
+                }
+                else if (brightness > maxb) {
+                    brightness = maxb;
+                }
+                System.out.println("The Average brightness is:" +brightness);
+                change(brightness);
+            }
+        }catch(Exception e){
+                    System.out.println("There was a problem" + e);
+                    }
+    }
+    // gets default webcam as variable.
+    static Webcam web = Webcam.getDefault();
+    
+    // updates webcam to drop down list choice.
+    static void webupd(Webcam webb) {
+        web = webb;
+    }
+    
+    // variable for type of run.
+    static boolean status = true;
+    
+    // updates status when called.
+    static void stateupd(boolean state) {
+        status = state;
+    }
+    
+    // alternate method to run program utilizing the time of day and the min/max brightnesses.
+    static void altrun(int t) {
+        int distance = maxb - minb;
+        System.out.println(t);
+        double iter = distance / 12;
+        System.out.println(iter);
+        int ival = (int) iter;
+        System.out.println(ival);
+       try {
+        if (t > 0 & t < 12) {
+            int bright = minb + ival * t;
+            change(bright);
+        }
+        else if (t == 12) {
+            change(maxb);
+        }
+        else if (t > 12 & t <=23) {
+            int hour = t - 12;
+            int bright = maxb - ival * hour;
+            change(bright);
+        }
+        else {
+            change(minb);
+        }
+       }
+       catch(Exception e){
+           System.out.println(e);
+       }
+    }
+    
+    // main run for the program, while looping, checks status of the run and runs respective processes to perform the program.
+    @Override
+    public void run() {
+        try {
+            while (keepRunning.get()) {
+                if (status == true) {
+                wait(1000);
+                web.open();
+                ImageIO.write(web.getImage(), "JPG", new File("webimg.jpg"));
+                File img = new File("webimg.jpg");
+                String path = img.getPath();
+                lum(path);
+                }
+                else if (status == false) {
+                    wait(1000);
+                    LocalTime time = java.time.LocalTime.now();
+                    int hour = time.getHour();
+                    altrun(hour);
+                }
+            }
+        }catch(Exception ex) {
+            System.out.println (ex.toString());
+        }
+    }
+
+}
---
+++ src/javaapplication9/front.java
@@ -0,0 +1,347 @@
+
+package javaapplication9;
+
+import com.github.sarxos.webcam.Webcam;
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.JOptionPane;
+import java.util.List;
+
+// class initializer extends JFrame
+public class front extends javax.swing.JFrame {
+
+    // Creates variable for Looper class
+private Looper looper;
+    
+// initializes JFrame components
+    public front() {
+        initComponents();
+    }  
+    
+    // creates variables for minimum and maximum, defaults are 1-100.
+static int maxb = 100;
+static int minb = 1;
+
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jLabel1 = new javax.swing.JLabel();
+        runB = new javax.swing.JButton();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        jTextField1 = new javax.swing.JTextField();
+        jLabel4 = new javax.swing.JLabel();
+        jTextField2 = new javax.swing.JTextField();
+        jButton1 = new javax.swing.JButton();
+        jButton2 = new javax.swing.JButton();
+        jComboBox1 = new javax.swing.JComboBox<>();
+        jLabel5 = new javax.swing.JLabel();
+        jButton3 = new javax.swing.JButton();
+        jButton4 = new javax.swing.JButton();
+        jCheckBox1 = new javax.swing.JCheckBox();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jLabel1.setFont(new java.awt.Font("Segoe UI", 0, 24)); // NOI18N
+        jLabel1.setText("Brightness Automator v1.0");
+
+        runB.setText("Start");
+        runB.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                runBActionPerformed(evt);
+            }
+        });
+
+        jLabel2.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel2.setText("Program stopped.");
+
+        jLabel3.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel3.setText("Maximum Brightness:");
+
+        jTextField1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jTextField1ActionPerformed(evt);
+            }
+        });
+
+        jLabel4.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel4.setText("Minimum Brightness:");
+
+        jTextField2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jTextField2ActionPerformed(evt);
+            }
+        });
+
+        jButton1.setText(">");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+
+        jButton2.setText(">");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
+
+        jComboBox1.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
+        jComboBox1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jComboBox1ActionPerformed(evt);
+            }
+        });
+
+        jLabel5.setText("Choose Active Webcam:");
+
+        jButton3.setText(">");
+        jButton3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton3ActionPerformed(evt);
+            }
+        });
+
+        jButton4.setText("Refresh List");
+        jButton4.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton4ActionPerformed(evt);
+            }
+        });
+
+        jCheckBox1.setSelected(true);
+        jCheckBox1.setText("Use Webcam");
+        jCheckBox1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jCheckBox1ActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(56, 56, 56)
+                        .addComponent(jLabel1))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(131, 131, 131)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(runB, javax.swing.GroupLayout.DEFAULT_SIZE, 131, Short.MAX_VALUE)
+                            .addComponent(jLabel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
+                .addContainerGap(62, Short.MAX_VALUE))
+            .addGroup(layout.createSequentialGroup()
+                .addGap(41, 41, 41)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(jLabel3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(jLabel4, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 90, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, 90, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 1, Short.MAX_VALUE)))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(jLabel5, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jButton3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addComponent(jButton4, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(jCheckBox1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addGap(41, 41, 41))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 36, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(27, 27, 27)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jLabel5)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jButton4, javax.swing.GroupLayout.PREFERRED_SIZE, 21, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jLabel3)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(jButton1)
+                            .addComponent(jTextField1))))
+                .addGap(4, 4, 4)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jButton3))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jLabel4)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(jTextField2)
+                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                        .addComponent(jButton2)
+                        .addComponent(jCheckBox1)))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 19, Short.MAX_VALUE)
+                .addComponent(runB, javax.swing.GroupLayout.PREFERRED_SIZE, 41, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 18, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(21, 21, 21))
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+    
+    // the wait method which intakes time as milliseconds and pauses the program for that long.  
+    public static void wait(int ms)
+    {
+        try
+        {
+            Thread.sleep(ms);
+        }
+        catch(InterruptedException ex)
+        {
+            Thread.currentThread().interrupt();
+        }
+    }
+    // when the run button is clicked, if the program is not looping, it begins looping and creates a Looper thread before disabling buttons.
+    private void runBActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_runBActionPerformed
+        if (looper == null) {
+            runB.setText("Stop");
+            jLabel2.setText("Program running...");
+            looper = new Looper();
+            Thread t = new Thread(looper);
+            Looper.maxupd(maxb);
+            Looper.minupd(minb);
+            jButton1.setEnabled(false);
+            jButton2.setEnabled(false);
+            jButton3.setEnabled(false);
+            t.start();
+            // If the thread is looping when runB is clicked, it stops the loop and resets buttons.
+        } else {
+            looper.stop();
+            looper = null;
+            runB.setText("Start");
+            jLabel2.setText("Program stopped.");
+            jButton1.setEnabled(true);
+            jButton2.setEnabled(true);
+            jButton3.setEnabled(true);
+        }
+    }//GEN-LAST:event_runBActionPerformed
+
+    private void jTextField1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField1ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jTextField1ActionPerformed
+
+    private void jTextField2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField2ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jTextField2ActionPerformed
+
+    // when the max brightness is confirmed, this method checks whether it is within the acceptable boundaries before updating it.
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        String txt = jTextField1.getText();
+        try{
+            int number = Integer.parseInt(txt);
+            if (number < 1 || number > 100){
+                JOptionPane.showMessageDialog(null, "Please enter a number from 1-100.");
+            }
+            else if (number < minb) {
+                number = minb + 1;
+                maxb = number;
+                JOptionPane.showMessageDialog(null, "Max number cannot be lower than min number.");
+            }
+            else {
+               System.out.println(number);
+               maxb = number;
+            }
+        }
+        catch (NumberFormatException ex){
+            JOptionPane.showMessageDialog(null, "Please enter a number from 1-100.");
+        }
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    // when the min brightness is confirmed, this method checks whether it is within the acceptable boundaries before updating it.
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        String txt = jTextField2.getText();
+        try{
+            int number = Integer.parseInt(txt);
+            if (number < 1 || number > 100){
+                JOptionPane.showMessageDialog(null, "Please enter a number from 1-100.");
+            }
+            else if (number > maxb) {
+                number = maxb - 1;
+                minb = number;
+                JOptionPane.showMessageDialog(null, "Min number cannot be higher than max number.");
+            }
+            else {
+               System.out.println(number);
+               minb = number;
+            }
+        }
+        catch (NumberFormatException ex){
+            JOptionPane.showMessageDialog(null, "Please enter a number from 1-100.");
+        }
+    }//GEN-LAST:event_jButton2ActionPerformed
+
+    // selects the currently selected webcam and updates the Looper thread with it.
+    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
+        Object choice = jComboBox1.getSelectedItem();
+        String n = choice.toString();
+        String name = n.substring(7);
+        List<Webcam> webs = Webcam.getWebcams();
+        for (Webcam w : webs) {
+            if (w.getName().equals(name)) {
+                Looper.webupd(w);
+            }
+        }
+    }//GEN-LAST:event_jButton3ActionPerformed
+
+    // refreshes the list of webcams
+    private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
+        List<Webcam> webs = Webcam.getWebcams();
+        jComboBox1.setModel(new DefaultComboBoxModel(webs.toArray(new Object[0])));
+    }//GEN-LAST:event_jButton4ActionPerformed
+
+    private void jComboBox1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jComboBox1ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jComboBox1ActionPerformed
+
+    // if checkbox is changed, updates looper method "state" with new status.
+    private void jCheckBox1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jCheckBox1ActionPerformed
+        boolean state = jCheckBox1.isSelected();
+        Looper.stateupd(state);
+    }//GEN-LAST:event_jCheckBox1ActionPerformed
+
+    // main - runs when program runs, initializes visible elements and quickly gets and appends a list of webcams to the combo box.
+    public static void main(String args[]) {
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new front().setVisible(true);
+            }
+        });
+        wait(100);
+        List<Webcam> webs = Webcam.getWebcams();
+        jComboBox1.setModel(new DefaultComboBoxModel(webs.toArray(new Webcam[0])));
+        
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
+    private javax.swing.JButton jButton3;
+    private javax.swing.JButton jButton4;
+    private javax.swing.JCheckBox jCheckBox1;
+    private static javax.swing.JComboBox<String> jComboBox1;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JTextField jTextField1;
+    private javax.swing.JTextField jTextField2;
+    private javax.swing.JButton runB;
+    // End of variables declaration//GEN-END:variables
+}
